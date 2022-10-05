650534f77c3de27d69776eefb8bd6028f01d4d0e
ALFONOVA <85473027+Agus1224@users.noreply.github.com>
2022-10-05 15:57:40 +0800

    Update empower.sh

--- EMPOWER/empower.sh
+++ EMPOWER/empower.sh
@@ -34,7 +34,9 @@ read Validator_Name
 echo "_|-_|-_|-_|-_|-_|-_|"
 echo export Validator_Name=$Validator_Name >> $HOME/.bash_profile
 echo export CHAIN_ID="altruistic-1" >> $HOME/.bash_profile
-echo export Wallet=$Wallet >> $HOME/.bash_profile
+if [ ! $Wallet ]; then
+	echo "export Wallet=wallet" >> $HOME/.bash_profile
+fi
 source ~/.bash_profile
 
 echo -e "\e[1m\e[32m1. Updating packages and dependencies--> \e[0m" && sleep 1
