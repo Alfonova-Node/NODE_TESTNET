c18399d5a947c2795cc191036f8c9a480d612678
ALFONOVA <85473027+Alfonova-Node@users.noreply.github.com>
2023-02-09 19:20:25 +0800

    Update massa-testnet.sh

--- MASSA LABS/massa-testnet.sh
+++ MASSA LABS/massa-testnet.sh
@@ -56,7 +56,7 @@ sudo apt-get install librocksdb-dev build-essential -y
 echo -e "\e[1m\e[32m3. Downloading and building massa binary... \e[0m" && sleep 1
 # download binary dan ekstrak
 cd $HOME
-wget https://github.com/massalabs/massa/releases/download/TEST.19.1/massa_TEST.19.1_release_linux.tar.gz
+wget https://github.com/massalabs/massa/releases/download/TEST.19.2/massa_TEST.19.1_release_linux.tar.gz
 tar xvzf massa_TEST.19.1_release_linux.tar.gz
 # cd massa/massa-node/base_config && rm -rf config.toml
 # wget https://raw.githubusercontent.com/mdlog/testnet-mdlog/main/config.toml
